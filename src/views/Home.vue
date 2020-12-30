70e0ad4f3189075e8e50151df85c0d9599ed3f60
ring920113 <ring920113@ring920eMacBook.lan>
2020-12-30 20:28:26 +0800

    origin

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,40 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+.whole
+  .h-icon
+  .t1
+    p 關於學院
+  .list1
+    li.content1 學院簡介
+    li.content1 未來展望
+    li.content1 姐妹學校
+  .t2
+    p 學院師資
+  .t3
+    p 學院規章
+  .t4
+    p 招生訊息
+  .t5
+    p 榮譽榜
+  .logo
+  .t6
+    p 活動花絮
+  .t7
+    p 藝院影音
+  .t8
+    p 藝術學報
+  .t9
+    p 聯絡資訊
+  .m-icon
+  .photo
+  .slide
+    .p1
+    .p2
+    .p3
+    .p4
+    .p5
+    .p6
+    .p7
+    .p8
 </template>
 
 <script>
@@ -16,3 +48,310 @@ export default {
   }
 }
 </script>
+
+<style lang="sass">
+.whole
+  position: relative
+  width: 1440px
+  height: 1024px
+  background: #FFFFFF
+.h-icon
+  background-image: url("../assets/home.png")
+  position: absolute
+  width: 40px
+  height: 40px
+  left: 36px
+  background-size: cover
+.t1
+  position: absolute
+  width: 109px
+  height: 40px
+  left: 153px
+  font-family: Yesteryear
+  font-style: normal
+  font-weight: normal
+  font-size: 12px
+  line-height: 15px
+  text-transform: uppercase
+  color: #000000
+  text-align:center
+  border-radius: 20px
+  background: #ffffff
+  box-shadow:  20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff
+  cursor: pointer
+  transition: all 0.2s ease-out
+.t1:hover
+  border-radius: 20px
+  background: linear-gradient(145deg, #e6e6e6, #ffffff)
+  box-shadow:  20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff
+#list1
+	background: white
+	text-align: center
+	width: 120px
+	list-style: none
+	line-height: 60px
+	border-radius: 5px
+	position: absolute
+	left: 135px
+	top: -120px
+	z-index: -1
+.t2
+  position: absolute
+  width: 109px
+  height: 40px
+  left: 302px
+  font-family: Yesteryear
+  font-style: normal
+  font-weight: normal
+  font-size: 12px
+  line-height: 15px
+  text-transform: uppercase
+  color: #000000
+  text-align:center
+  border-radius: 20px
+  background: #ffffff
+  box-shadow:  20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff
+  cursor: pointer
+  transition: all 0.2s ease-out
+.t2:hover
+  border-radius: 20px
+  background: linear-gradient(145deg, #e6e6e6, #ffffff)
+  box-shadow:  20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff
+.t3
+  position: absolute
+  width: 109px
+  height: 40px
+  left: 451px
+  font-family: Yesteryear
+  font-style: normal
+  font-weight: normal
+  font-size: 12px
+  line-height: 15px
+  text-transform: uppercase
+  color: #000000
+  text-align:center
+  border-radius: 20px
+  background: #ffffff
+  box-shadow:  20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff
+  cursor: pointer
+  transition: all 0.2s ease-out
+.t3:hover
+  border-radius: 20px
+  background: linear-gradient(145deg, #e6e6e6, #ffffff)
+  box-shadow:  20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff
+.t4
+  position: absolute
+  width: 109px
+  height: 40px
+  left: 600px
+  font-family: Yesteryear
+  font-style: normal
+  font-weight: normal
+  font-size: 12px
+  line-height: 15px
+  text-transform: uppercase
+  color: #000000
+  text-align:center
+  border-radius: 20px
+  background: #ffffff
+  box-shadow:  20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff
+  cursor: pointer
+  transition: all 0.2s ease-out
+.t4:hover
+  border-radius: 20px
+  background: linear-gradient(145deg, #e6e6e6, #ffffff)
+  box-shadow:  20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff
+.t5
+  position: absolute
+  width: 109px
+  height: 40px
+  left: 749px
+  font-family: Yesteryear
+  font-style: normal
+  font-weight: normal
+  font-size: 12px
+  line-height: 15px
+  text-transform: uppercase
+  color: #000000
+  text-align:center
+  border-radius: 20px
+  background: #ffffff
+  box-shadow:  20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff
+  cursor: pointer
+  transition: all 0.2s ease-out
+.t5:hover
+  border-radius: 20px
+  background: linear-gradient(145deg, #e6e6e6, #ffffff)
+  box-shadow:  20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff
+.logo
+  background-image: url("../assets/nthuart.png")
+  position: absolute
+  width: 650px
+  height: 650px
+  left: 8px
+  top: -30px
+  background-size: cover
+.t6
+  position: absolute
+  width: 109px
+  height: 40px
+  left: 136px
+  top: 550px
+  font-family: Yesteryear
+  font-style: normal
+  font-weight: normal
+  font-size: 12px
+  line-height: 15px
+  text-transform: uppercase
+  color: #000000
+  text-align:center
+  border-radius: 20px
+  background: #ffffff
+  box-shadow:  20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff
+  cursor: pointer
+  transition: all 0.2s ease-out
+.t6:hover
+  border-radius: 20px
+  background: linear-gradient(145deg, #e6e6e6, #ffffff)
+  box-shadow:  20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff
+.t7
+  position: absolute
+  width: 109px
+  height: 40px
+  left: 285px
+  top: 550px
+  font-family: Yesteryear
+  font-style: normal
+  font-weight: normal
+  font-size: 12px
+  line-height: 15px
+  text-transform: uppercase
+  color: #000000
+  text-align:center
+  border-radius: 20px
+  background: #ffffff
+  box-shadow:  20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff
+  cursor: pointer
+  transition: all 0.2s ease-out
+.t7:hover
+  border-radius: 20px
+  background: linear-gradient(145deg, #e6e6e6, #ffffff)
+  box-shadow:  20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff
+.t8
+  position: absolute
+  width: 109px
+  height: 40px
+  left: 434px
+  top: 550px
+  font-family: Yesteryear
+  font-style: normal
+  font-weight: normal
+  font-size: 12px
+  line-height: 15px
+  text-transform: uppercase
+  color: #000000
+  text-align:center
+  border-radius: 20px
+  background: #ffffff
+  box-shadow:  20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff
+  cursor: pointer
+  transition: all 0.2s ease-out
+.t8:hover
+  border-radius: 20px
+  background: linear-gradient(145deg, #e6e6e6, #ffffff)
+  box-shadow:  20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff
+.t9
+  position: absolute
+  width: 109px
+  height: 40px
+  left: 583px
+  top: 550px
+  font-family: Yesteryear
+  font-style: normal
+  font-weight: normal
+  font-size: 12px
+  line-height: 15px
+  text-transform: uppercase
+  color: #000000
+  text-align:center
+  border-radius: 20px
+  background: #ffffff
+  box-shadow:  20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff
+  cursor: pointer
+  transition: all 0.2s ease-out
+.t9:hover
+  border-radius: 20px
+  background: linear-gradient(145deg, #e6e6e6, #ffffff)
+  box-shadow:  20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff
+.m-icon
+  background-image: url("../assets/max.png")
+  position: absolute
+  width: 40px
+  height: 40px
+  left: 36px
+  top: 550px
+  background-size: cover
+.photo
+  background-image: url("../assets/circle.png")
+  position: absolute
+  width: 1280px
+  height: 720px
+  top: -90px
+  background-size: cover
+.p1
+  background-image: url("../assets/w-1.png")
+  position: absolute
+  width: 1280px
+  height: 720px
+  top: -90px
+  background-size: cover
+.p2
+  background-image: url("../assets/w-2.png")
+  position: absolute
+  width: 1280px
+  height: 720px
+  top: -90px
+  background-size: cover
+.p3
+  background-image: url("../assets/w-3.png")
+  position: absolute
+  width: 1280px
+  height: 720px
+  top: -90px
+  background-size: cover
+.p4
+  background-image: url("../assets/w-4.png")
+  position: absolute
+  width: 1280px
+  height: 720px
+  top: -90px
+  background-size: cover
+.p5
+  background-image: url("../assets/w-5.png")
+  position: absolute
+  width: 1280px
+  height: 720px
+  top: -90px
+  background-size: cover
+.p6
+  background-image: url("../assets/w-6.png")
+  position: absolute
+  width: 1280px
+  height: 720px
+  top: -90px
+  background-size: cover
+.p7
+  background-image: url("../assets/w-7.png")
+  position: absolute
+  width: 1280px
+  height: 720px
+  top: -90px
+  background-size: cover
+.p8
+  background-image: url("../assets/w-8.png")
+  position: absolute
+  width: 1280px
+  height: 720px
+  top: -90px
+  background-size: cover
+</style>
